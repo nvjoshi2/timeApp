0881bac034ad1c6f66720e2347a5fd9f477a7c3d
Nathan Joshi <nvj1300@gmail.com>
2020-04-22 14:21:04 -0500

    remove static serving part of server.js will do seperately

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/taskInstances', taskInstancesRouter);
     //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     // })
 // }
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 // const port = process.env.PORT || 5000;
 const port = 3000
 // console.log(port)
