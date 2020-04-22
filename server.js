0368e11c0b6b01c91129318610ac392bad33b450
Nathan Joshi <nvj1300@gmail.com>
2020-04-22 03:24:12 -0500

    fixed typo

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/taskInstances', taskInstancesRouter);
     app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 // }
 // app.use(express.static(path.join(__dirname, "client/build")));
