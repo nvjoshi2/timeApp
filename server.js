58a370f54e37d8b96826bf293519cfa13432217f
Nathan Joshi <nvj1300@gmail.com>
2020-04-22 03:25:33 -0500

    reverted back static serving method

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ app.use('/api/taskInstances', taskInstancesRouter);
 
 // if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'))
+    // app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    // })
 // }
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 // const port = process.env.PORT || 5000;
 const port = 3000
 // console.log(port)
